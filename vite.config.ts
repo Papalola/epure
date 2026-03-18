35cea14118c6f875f3121f494f72a65d1c306b9f
Papalola <dbadin500@gmail.com>
2026-03-18 17:47:39 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
 export default defineConfig(({ mode }) => ({
-  base: "/epure/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
